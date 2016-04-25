8ceae08e4ebfc654b767806853b135a5959d6de1
rotkcraft <ravtec123@gmail.com>
2016-04-25 15:51:09 -0600

    Ejemplo Empleado Agregado

---
+++ src/Empleado.java
@@ -0,0 +1,52 @@
+/**
+ * Created by rcraft on 04-25-16.
+ */
+public class Empleado
+{
+    private String id;
+    private String nombre;
+    private double sueldo;
+
+    public Empleado(String id, String nombre, double sueldo)
+    {
+        this.id = id;
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+    }
+
+    public String getId()
+    {
+        return id;
+    }
+
+    public void setId(String id)
+    {
+        this.id = id;
+    }
+
+    public String getNombre()
+    {
+        return nombre;
+    }
+
+    public void setNombre(String nombre)
+    {
+        this.nombre = nombre;
+    }
+
+    public double getSueldo()
+    {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo)
+    {
+        this.sueldo = sueldo;
+    }
+
+    @Override
+    public String toString()
+    {
+        return  "id='" + id  + ", nombre='" + nombre +", sueldo=" + sueldo ;
+    }
+}
--- srctest/ColaTest.java
+++ srctest/ColaTest.java
@@ -19,8 +19,48 @@ public class ColaTest
         Archivo archivo=new Archivo("archivo/datos.csv");
         for(String linea:archivo.traerArchivo())
         {
+            System.out.println(linea.split(",")[2]);
             pila.apilar(linea);
             cola.encolar(linea);
+
+
+        }
+        System.out.println(pila);
+        System.out.println(pila.cima());
+        System.out.println(pila.desapilar());
+        System.out.println(pila.desapilar());
+        System.out.println(pila.desapilar());
+        System.out.println(pila.desapilar());
+        System.out.println(pila);
+
+        System.out.println(cola);
+        System.out.println(cola.cima());
+        System.out.println(cola.descolar());
+        System.out.println(cola.descolar());
+        System.out.println(cola.descolar());
+        System.out.println(cola.descolar());
+        System.out.println(cola);
+
+
+
+
+
+    }
+
+    @Test
+    public void probandoDos() throws Exception
+    {
+        Pila<Empleado> pila=new Pila<Empleado>();
+        Cola<Empleado> cola=new Cola<Empleado>();
+
+
+        Archivo archivo=new Archivo("archivo/datos.csv");
+        for(String linea:archivo.traerArchivo())
+        {
+            String t[]=linea.substring(0,linea.length()-1).split(",");
+
+            pila.apilar(new Empleado(t[0],t[1],Double.parseDouble(t[2])));
+            cola.encolar(new Empleado(t[0],t[1],Double.parseDouble(t[2])));
 
 
         }
